1812634ccc99613904be91c1f8e3888ee2cba20d
gmel <ganmel@gmail.com>
2023-04-26 14:41:13 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,25 +1,108 @@
-var SDK = lpTag.agentSDK || {};
-SDK.init({});
+ function setConsumerProfileOnUpdate() {
 
-var onSuccess = function() {
-    // Do something with the success
-    console.log("Success")
-};
+ 
 
-var onError = function(err) {
-    console.log("Error occurred",err)
-};
+            document.getElementById("lblLPError").style.visibility = "visible";
 
+            document.getElementById("lblLPError").style.color = "black";
 
-var consumerData = {
-    avatarUrl:"",
-    backgndImgUri:"",
-    emailAddress: "",
-    firstName: "",
-    lastName: "",
-    phone: ""
-};
+            document.getElementById("lblLPError").innerText = "Update notification sent to LivePerson";
 
-function updateProfile() {
-    SDK.setConsumerProfile(consumerData, onSuccess, onError)
+           
+
+            var notificationHandler = function (data) {
+
+                // Do something with the notifications
+
+            };
+
+ 
+
+            var focusHandler = function () {
+
+                // Do something when the visitor is focused
+
+                // Assume the visitor is focused to begin with
+
+            };
+
+ 
+
+            var blurHandler = function () {
+
+                // Do something when the visitor is blurred
+
+                // Assume the visitor is focused to begin with
+
+            };
+
+ 
+
+ 
+
+            lpTag.agentSDK.init({
+
+                notificationCallback: notificationHandler,
+
+                visitorFocusedCallback: focusHandler,
+
+                visitorBlurredCallback: blurHandler
+
+            });
+
+ 
+
+            var onSuccess = function () {
+
+                document.getElementById("lblLPError").style.color = "black";
+
+                document.getElementById("lblLPError").innerText = "Notification sent to LivePerson for chat update";
+
+                // Do something with the success
+
+            };
+
+ 
+
+            var onError = function (err) {
+
+                debugger;
+
+                document.getElementById("lblLPError").style.color = "red";
+
+                //document.getElementById("lblLPError").innerText = "Error occurred in sending notification to LivePerson";
+
+                document.getElementById("lblLPError").innerText = err;
+
+ 
+
+                // Do something with the error
+
+            };
+
+            var phval;
+
+            phval = "bb";
+
+ 
+
+            var consumerData = {
+
+                avatarUrl: "",
+
+                backgndImgUri: "",
+
+                email: "",
+
+                firstName: "",
+
+                lastName: "",
+
+                phone: phval
+
+            };
+
+            lpTag.agentSDK.setConsumerProfile(consumerData, onSuccess, onError);
+
+ 
 }
